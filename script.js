3c715eea03148cb79b6689ae3e946f26aeae0fe9
JotaMacLane <jpnascimentoa@gmail.com>
2024-09-10 10:23:30 -0300

    Atualizando projeto

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
 const btnMenu = document.querySelector(".fa-bars");
-const header = document.querySelector(".header");
-const navText = document.querySelectorAll(".text-link");
+const header = document.querySelector("header");
 
-btnMenu.addEventListener("click", openMenu);
-
-function openMenu() {
-    header.classList.add("expande-menu");
-    navText.forEach(element => {
-        element.classList.add("open-menu");
-    });
-}
-
-function closeMenu() {
-    header.style.width = "100px";
-    navText.forEach(element => {
-        element.style.display = "none";
-    });
-}
+btnMenu.addEventListener("click", () => {
+    // Alterna a classe que expande o menu
+    header.classList.toggle("expande-menu");
+});
